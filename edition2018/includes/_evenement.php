3ee2f55facd30f98b77ff3a77ff588b2fa1a0d0d
elina207 <44774541+elina207@users.noreply.github.com>
2018-11-13 14:10:10 +0100

    [evenement] version ordinateur

--- edition2018/includes/_evenement.php
+++ edition2018/includes/_evenement.php
@@ -1,5 +1,40 @@
 <section class="evenement">
 
+  <div class="wrap1">
 
-  
+	<div class="logo_menu">
+		<div class="logo">
+			<img class="logo_mmietapres" src="images/mmietapres.png">
+		</div>
+		<div class="menu">
+			<nav>
+				<li>Présentation</li>
+				<li>Programme</li>
+				<li>Ils seront là</li>
+				<li>Edition 2018</li>
+				<li>S'inscrire</li>
+				<li>Contact</li>
+			</nav>
+		</div>
+	</div>
+
+	<div class="presentation">
+		<h1>L'événement</h1>
+		<p>MMI&APRES est l'événement qui réunit étudiants et professionnels.</p>
+		<p>Chers professionnels, n'attendez plus et venez rencontrer les étudiants le 17 Janvier 2019 à l'IUT de Chambéry à partir de 8h30.</p>
+		<p>Un petit déjeuner conivival, une matinée  de stage dating, des tables rondes et beaucoup d'autres surprises vous attendent.</p>
+		<p>C'est l'occasion rêvée de faire des rencontres et de décrocher un stagiaire.</p>
+
+		<h2>Inscription</h2>
+		<div class="boutons_inscription">
+			<div class="inscription_etudiants">
+				<input class="bouton_inscritpion_etudiants" type="button" value="Etudiants">
+			</div>
+			<div class="inscription_professionels">
+				<input class="bouton_inscritpion_professionnels" type="button" value="Professionnels">
+			</div>
+		</div>
+	</div>
+	</div>
+
 </section>
