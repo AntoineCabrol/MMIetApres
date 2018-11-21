54172c7f0aca56dfeb2dc4bff0d2815061581775
Antoine Cabrol <contact@antoinecabrol.com>
2018-11-21 13:21:33 +0100

    [splashscreen] correctif Firefox

--- edition2018/includes/_etudiants.php
+++ edition2018/includes/_etudiants.php
@@ -1,5 +1,53 @@
 <section class="etudiants">
+  <div class="wrap1">
+    <h1>Témoignages</h1>
+    <div class="flex_temoignage">
+      <div class="temoignage_block">
+          <div class="temoignage_photo_fond">
+          </div>
+          <div class="temoignage_contenu">
+                  <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+                  <h3>Audiovisuel</h3>
+                  <p class="temoignage_texte">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+          </div>
+      </div>
+
+      <div class="temoignage_block">
+          <div class="temoignage_photo_fond">
+          </div>
+          <div class="temoignage_contenu">
+                  <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+                  <h3>Programmation</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+          </div>
+      </div>
+
+      <div class="temoignage_block">
+          <div class="temoignage_photo_fond">
+          </div>
+          <div class="temoignage_contenu">
+              <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+              <h3>Graphisme</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+          </div>
+      </div>
+
+      <div class="temoignage_block">
+          <div class="temoignage_photo_fond">
+          </div>
+          <div class="temoignage_contenu">
+                  <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+                  <h3>Communication</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+          </div>
+      </div>
+    </div>
+    <div class="flex_button_tem">
+      <a class="bouton inscription_etudiants">Étudiants</a>
+      <a class="bouton inscription_professionels">Professionnels</a>
+    </div>
+  </div>
 
 
-  
+
 </section>
--- edition2018/includes/_programme.php
+++ edition2018/includes/_programme.php
@@ -5,37 +5,47 @@
 
   		<div class="programme_gauche" id="accueil">
   			<img id="image_accueil" src="images/accueil.svg">
-  			<h4>8h30 - 9h</h4>
+        <div class="contenu">
+        <h4>8h30 - 9h</h4>
   			<h3>Accueil petit dej'</h3>
   			<p>Nous vous accueillons autour d'un café et d'un petit dejeuné pour faire connaissance dans la bonne humeur</p>
-  		</div>
+      </div>
+      </div>
 
       <div class="programme_droit" id="presentation_dut">
         <img id="image_presentation_dut" src="images/presentation_dut.svg">
+        <div class="contenu">
         <h4>9h - 9h15</h4>
         <h3>Présentation DUT MMI</h3>
         <p>Nous vous présentons la formation ainsi que ses objectifs</p>
       </div>
+      </div>
 
       <div class="programme_gauche" id="stage_dating">
         <img id="image_stage_dating" src="images/stage_dating.svg">
+        <div class="contenu">
         <h4>9h30 - 12h</h4>
         <h3>Stage-dating</h3>
         <p>C'est le moment pour vous d'échanger avec les étudiants et de décrocher un stagiaire</p>
       </div>
+      </div>
 
       <div class="programme_droit" id="buffet">
         <img id="image_buffet" src="images/buffet.svg">
+        <div class="contenu">
         <h4>12h - 13h30</h4>
         <h3>Buffet</h3>
         <p>Vous pouvez maintenant vous rassasier tout en rencontrant des professionels dans la convivialité</p>
+        </div>
       </div>
 
       <div class="programme_gauche" id="tables_rondes">
         <img id="image_tables_rondes" src="images/tables_rondes.svg">
+        <div class="contenu">
         <h4>13h30 - 15h30</h4>
         <h3>Tables rondes</h3>
         <p>Echangez entre étudiants et professionnels sur des sujets d'actualité</p>
+        </div>
       </div>
 
   	</div>
