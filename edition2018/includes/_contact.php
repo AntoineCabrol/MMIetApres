d6ce99df4f260ae7823f352e5a3cbade9539d0d9
Antoine Cabrol <contact@antoinecabrol.com>
2018-11-21 17:41:29 +0100

    maj

--- edition2018/includes/_contact.php
+++ edition2018/includes/_contact.php
@@ -4,7 +4,7 @@
     <div class="flex_contact_gauche">
         <div class="flex_mail">
             <p>mmietapres_iutchambery@gmail.com</p>
-            <img/>
+            <div><img src="./images/icone_facebook.png" width="25px"/></div>
             <img/>
         </div>
         <div class="flex_depart">
@@ -27,5 +27,5 @@
     </div>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.4158911757763!2d5.868904015823816!3d45.6424692295509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478b09cbb2276115%3A0x3d15d003b27c5cce!2sIUT+de+Chamb%C3%A9ry!5e0!3m2!1sfr!2sfr!4v1542229141830" width="400" height="250" frameborder="0" style="border:0" display="block" allowfullscreen></iframe>
 </div>
-<p>Lorem Lorem</p>
+<p class="contact_mentions">©2018 - Mentions Légales</p>
 </footer>
--- edition2018/includes/_etudiants.php
+++ edition2018/includes/_etudiants.php
@@ -1,14 +1,14 @@
 <section class="etudiants">
   <div class="wrap1">
-    <h1>Témoignages</h1>
+    <h1 class="h_etudiants">Témoignages</h1>
     <div class="flex_temoignage">
       <div class="temoignage_block">
           <div class="temoignage_photo_fond">
           </div>
           <div class="temoignage_contenu">
-                  <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+                  <img src="./images/icone_audiovisuel.svg" class="temoignage_icone"/>
                   <h3>Audiovisuel</h3>
-                  <p class="temoignage_texte">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+                  <p class="temoignage_texte">« J’avais trouvé un stage dans une boite de production de pub à Paris, mais j’ai quand même souhaité me mesurer au stage-dating que proposait MMI&Après. J’ai fais la rencontre de Julien, directeur général et réalisateur de l’Agence Future. Je me suis rendue compte que nos attentes quant à la place du stagiaire étaient similaires et que les missions qu'il attendait me correspondaient. Quelques heures plus tard, Julien me recontactait pour me proposer de faire parti de l’aventure ! »</p>
           </div>
       </div>
 
@@ -16,9 +16,9 @@
           <div class="temoignage_photo_fond">
           </div>
           <div class="temoignage_contenu">
-                  <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+                  <img src="./images/icone_programmation.svg" class="temoignage_icone"/>
                   <h3>Programmation</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+                  <p>« J’avais déjà repéré l’entreprise NewQuest avant l’événement, mais c’est pendant MMI&Après que j’ai passé mon stage-dating. Un peu de pression et pas mal d’échanges avec les entreprises, qui m’ont permis de décrocher mon stage à Newquest, et ainsi obtenir une alternance ! »</p>
           </div>
       </div>
 
@@ -26,9 +26,9 @@
           <div class="temoignage_photo_fond">
           </div>
           <div class="temoignage_contenu">
-              <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+              <img src="./images/icone_graphisme.svg" class="temoignage_icone"/>
               <h3>Graphisme</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+              <p>« Je suis allé à MMI&Après afin de trouver un stage car il y avait des agences qui m'intéressaient. J'ai pu passer quatre entretiens qui m'ont permis de discuter avec des professionels. Deux professionnels étaient intéressés. J'ai pu visiter leur boite et repasser un entretien plus long. MMI&Après permet d'avoir des rendez-vous avec les agences sans avoir à passer des heures à les chercher. De plus cela permet d'avoir des contacts. »</p>
           </div>
       </div>
 
@@ -36,9 +36,9 @@
           <div class="temoignage_photo_fond">
           </div>
           <div class="temoignage_contenu">
-                  <img src="images/icone_audiovisuel.svg" class="temoignage_icone"/>
+                  <img src="./images/icone_communication.svg" class="temoignage_icone"/>
                   <h3>Communication</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis hendrerit lacus nec mi sagittis placerat. Integer convallis nisi eu lorem ultricies volutpat. Nam efficitur nibh tortor, sit amet ultricies felis vestibulum id. Sed imperdiet felis vitae mauris tempus vehicula. Proin condimentum neque in lectus volutpat feugiat. Donec tincidunt, est sit amet varius pharetra, ipsum augue ornare quam, at consequat felis massa ut metus. Aliquam erat volutpat. Quisque at mi lobortis</p>
+                  <p>« MMI&Après a été une excellente occasion de mettre un premier pas dans le monde professionnel ! En plus d’avoir trouvé mon stage, l’événement m’a permis de prendre davantage confiance en moi. »</p>
           </div>
       </div>
     </div>
