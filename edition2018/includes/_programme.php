a412b2229641db726455994d1866122e46abd556
elina207 <44774541+elina207@users.noreply.github.com>
2018-11-21 22:06:20 +0100

    Modif evenement et programme

--- edition2018/includes/_programme.php
+++ edition2018/includes/_programme.php
@@ -4,47 +4,47 @@
   		<h1>Programme</h1>
 
   		<div class="programme_gauche" id="accueil">
-  			<img id="image_accueil" src="images/accueil.svg">
+  			<img class="image_programme" id="image_accueil" src="images/accueil.svg">
         <div class="contenu">
         <h4>8h30 - 9h</h4>
-  			<h3>Accueil petit dej'</h3>
-  			<p>Nous vous accueillons autour d'un café et d'un petit dejeuné pour faire connaissance dans la bonne humeur</p>
+  			<h3>Accueil et café</h3>
+  			<p>Nous vous accueillons autour d’un café pour bien commencer la journée</p>
       </div>
       </div>
 
       <div class="programme_droit" id="presentation_dut">
-        <img id="image_presentation_dut" src="images/presentation_dut.svg">
+        <img class="image_programme" id="image_presentation_dut" src="images/presentation_dut.svg">
         <div class="contenu">
         <h4>9h - 9h15</h4>
-        <h3>Présentation DUT MMI</h3>
-        <p>Nous vous présentons la formation ainsi que ses objectifs</p>
+        <h3>Présentation de notre formation</h3>
+        <p>Qu’est-ce que MMI et quels sont nos objectifs ?</p>
       </div>
       </div>
 
       <div class="programme_gauche" id="stage_dating">
-        <img id="image_stage_dating" src="images/stage_dating.svg">
+        <img class="image_programme" id="image_stage_dating" src="images/stage_dating.svg">
         <div class="contenu">
         <h4>9h30 - 12h</h4>
         <h3>Stage-dating</h3>
-        <p>C'est le moment pour vous d'échanger avec les étudiants et de décrocher un stagiaire</p>
+        <p>Mise en place de petits entretiens qui permettront l’échange entre professionnel et étudiant</p>
       </div>
       </div>
 
       <div class="programme_droit" id="buffet">
-        <img id="image_buffet" src="images/buffet.svg">
+        <img class="image_programme" id="image_buffet" src="images/buffet.svg">
         <div class="contenu">
         <h4>12h - 13h30</h4>
-        <h3>Buffet</h3>
-        <p>Vous pouvez maintenant vous rassasier tout en rencontrant des professionels dans la convivialité</p>
+        <h3>Repas / Buffet</h3>
+        <p>Pause pour reprendre des forces, c’est le moment pour échanger et rencontrer d’autres entreprises/étudiants</p>
         </div>
       </div>
 
       <div class="programme_gauche" id="tables_rondes">
-        <img id="image_tables_rondes" src="images/tables_rondes.svg">
+        <img class="image_programme" id="image_tables_rondes" src="images/tables_rondes.svg">
         <div class="contenu">
         <h4>13h30 - 15h30</h4>
         <h3>Tables rondes</h3>
-        <p>Echangez entre étudiants et professionnels sur des sujets d'actualité</p>
+        <p>Moment d’échanges interactifs entre professionnels et étudiants autour d’une thématique définie</p>
         </div>
       </div>
 
