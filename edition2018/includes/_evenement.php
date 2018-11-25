7ba14951f9f81a9705850bf72bff4fd101a60af1
elina207 <44774541+elina207@users.noreply.github.com>
2018-11-25 14:40:11 +0100

    [evenement] responsive

--- edition2018/includes/_evenement.php
+++ edition2018/includes/_evenement.php
@@ -3,16 +3,16 @@
   <div class="wrap1">
 
 	<div class="presentation">
-		<h1>L'événement</h1>
+		<h1 class="deskOnly">L'événement</h1>
 		<p>MMI&APRÈS est un événement qui permet une rencontre entre étudiants et professionnels.</p>
 		<p>Les professionnels peuvent découvrir la formation MMI et rencontrer d’autres entreprises. </p>
 		<p>Les étudiants, quant à eux, passent des entretiens pour trouver un stage de fin d’année. </p>
 		<p>Les tables rondes permettront aux professionnels tout comme aux étudiants de s’exprimer sur un sujet en rapport avec la formation MMI !</p>
 
-		<h2>Inscription</h2>
-		<div class="boutons_inscription">
-			<a class="bouton inscription_etudiants">Etudiants</a>
-			<a class="bouton inscription_professionels">Professionnels</a>
+		<h2 class="deskOnly">Inscription</h2>
+		<div class="boutons_inscription deskOnly">
+			<a class="bouton inscription_etudiants deskOnly">Etudiants</a>
+			<a class="bouton inscription_professionels deskOnly">Professionnels</a>
 		</div>
 	</div>
 	</div>
