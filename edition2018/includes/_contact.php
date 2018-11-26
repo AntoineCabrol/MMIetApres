e8895efac53ba822b0adb51f78be8c7123f01f3a
Antoine Cabrol <contact@antoinecabrol.com>
2018-11-26 17:29:29 +0100

    [contact] version desktop refaite

--- edition2018/includes/_contact.php
+++ edition2018/includes/_contact.php
@@ -1,31 +1,35 @@
 <footer class="contact">
-  <h1 class="h_footer">Contact</h1>
-<div class="flex_footer_global">
-    <div class="flex_contact_gauche">
-        <div class="flex_mail">
-            <p>mmietapres_iutchambery@gmail.com</p>
-            <div><img src="./images/icone_facebook.png" width="25px"/></div>
-            <img/>
+
+  <h1 class="deskOnly h_footer">Contact</h1>
+
+  <div class="flex_footer_global">
+      <div class="flex_coordonnees">
+        <p class="mail">mmietapres_iutchambery@gmail.com</p>
+        <p class="telephone">06 00 00 00 00</p>
+        <div class="icone_facebook"><img src="./images/icone_facebook.png" width="100%"/></div>
+      </div>
+      <div class="flex_depart">
+          <p>Département MMI</p>
+          <p>IUT de Chambéry Université de Savoie</p>
+          <p>Campus Scientifique - Savoie Technolac</p>
+          <p>73376 Le Bourget du Lac Cedex</p>
+      </div>
+      <div class="flex_img">
+        <div class="emetteur">
+          <img src="../images/mmi.png" height="100%"/>
         </div>
-        <div class="flex_depart">
-            <p>Département MMI</p>
-            <p>IUT de Chambéry Université de Savoie</p>
-            <p>Campus Scientifique - Savoie Technolac</p>
-            <p>73376 Le Bourget du Lac Cedex</p>
+        <div class="emetteur">
+          <img src="../images/neon.svg" height="100%"/>
         </div>
-        <div class="flex_img">
-            <div>
-            <img src="../images/mmi.png" width="70px"/>
-            </div>
-            <div>
-            <img src="../images/neon.svg" width="70px"/>
-            </div>
-            <div>
-            <img src="../images/iut.png" width="70px"/>
-            </div>
+        <div class="emetteur">
+          <img src="../images/iut.png" height="100%"/>
         </div>
-    </div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.4158911757763!2d5.868904015823816!3d45.6424692295509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478b09cbb2276115%3A0x3d15d003b27c5cce!2sIUT+de+Chamb%C3%A9ry!5e0!3m2!1sfr!2sfr!4v1542229141830" width="400" height="250" frameborder="0" style="border:0" display="block" allowfullscreen></iframe>
-</div>
-<p class="contact_mentions">©2018 - Mentions Légales</p>
+      </div>
+      <iframe class="googlemaps"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.4158911757763!2d5.868904015823816!3d45.6424692295509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478b09cbb2276115%3A0x3d15d003b27c5cce!2sIUT+de+Chamb%C3%A9ry!5e0!3m2!1sfr!2sfr!4v1542229141830"
+              frameborder="0" allowfullscreen></iframe>
+  </div>
+
+  <p class="contact_mentions">©2018 - <a href="mentions.php">Mentions Légales</a></p>
+
 </footer>
