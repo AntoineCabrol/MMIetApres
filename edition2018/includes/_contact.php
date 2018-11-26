14c191d9f1b48c6e330eb0c68006ff54398f7de1
Antoine Cabrol <contact@antoinecabrol.com>
2018-11-26 17:49:08 +0100

    [contact] responsive 30%

--- edition2018/includes/_contact.php
+++ edition2018/includes/_contact.php
@@ -28,6 +28,7 @@
       <iframe class="googlemaps"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.4158911757763!2d5.868904015823816!3d45.6424692295509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478b09cbb2276115%3A0x3d15d003b27c5cce!2sIUT+de+Chamb%C3%A9ry!5e0!3m2!1sfr!2sfr!4v1542229141830"
               frameborder="0" allowfullscreen></iframe>
+      <a class="mobileOnly bouton" href="#" target="_blank">Itinéraire</a>
   </div>
 
   <p class="contact_mentions">©2018 - <a href="mentions.php">Mentions Légales</a></p>
