9c08d31b84dda1822462437f910b2df4702479b8
BertrandPassieux <jean-francois.passieux@orange.fr>
2018-11-27 17:33:06 +0100

    [etudiant]barre_colorées_mignonnes + transistions

--- edition2018/includes/_etudiants.php
+++ edition2018/includes/_etudiants.php
@@ -8,14 +8,15 @@
     <div class="temoignage_block sliderMob__elem" data-S4="audiovisuel">
         <div class="temoignage_photo_fond sliderMob__logo"></div>
         <div class="temoignage_contenu sliderMob__texte">
-            <img src="./images/icone_audiovisuel.svg" class="deskOnly temoignage_icone"/>
+            <span class="deskOnly barre_couleur" data-color="bleu"></span>
+            <img src="./images/icone_audiovisuel.svg" class="deskOnly temoignage_icone" width="100%"/>
             <h3 class="deskOnly">Audiovisuel</h3>
             <p class="temoignage_texte">
               « J’avais trouvé un stage dans une boite de production de pub à
               Paris, mais j’ai quand même souhaité me mesurer au stage-dating que
               proposait MMI&Après. J’ai fais la rencontre de Julien, directeur
               général et réalisateur de l’Agence Future. Je me suis rendue compte
-              que nos attentes quant à la place du stagiaire étaient similaires et
+              que nos attentes et
               que les missions qu'il attendait me correspondaient. Quelques heures
               plus tard, Julien me recontactait pour me proposer de faire parti de
               l’aventure ! »
@@ -26,7 +27,8 @@
         <div class="temoignage_photo_fond sliderMob__logo">
         </div>
         <div class="temoignage_contenu sliderMob__texte">
-                <img src="./images/icone_programmation.svg" class="deskOnly temoignage_icone"/>
+                <span class="deskOnly barre_couleur" data-color="vert"></span>
+                <img src="./images/icone_programmation.svg" class="deskOnly temoignage_icone" width="100%"/>
                 <h3 class="deskOnly">Programmation</h3>
                 <p>« J’avais déjà repéré l’entreprise NewQuest avant l’événement, mais c’est pendant MMI&Après que j’ai passé mon stage-dating. Un peu de pression et pas mal d’échanges avec les entreprises, qui m’ont permis de décrocher mon stage à Newquest, et ainsi obtenir une alternance ! »</p>
         </div>
@@ -35,7 +37,8 @@
         <div class="temoignage_photo_fond sliderMob__logo">
         </div>
         <div class="temoignage_contenu sliderMob__texte">
-            <img src="./images/icone_graphisme.svg" class="deskOnly temoignage_icone"/>
+            <span class="deskOnly barre_couleur" data-color="magenta"></span>
+            <img src="./images/icone_graphisme.svg" class="deskOnly temoignage_icone" width="100%"/>
             <h3 class="deskOnly">Graphisme</h3>
             <p>« Je suis allé à MMI&Après afin de trouver un stage car il y avait des agences qui m'intéressaient. J'ai pu passer quatre entretiens qui m'ont permis de discuter avec des professionels. Deux professionnels étaient intéressés. J'ai pu visiter leur boite et repasser un entretien plus long. MMI&Après permet d'avoir des rendez-vous avec les agences sans avoir à passer des heures à les chercher. De plus cela permet d'avoir des contacts. »</p>
         </div>
@@ -44,7 +47,8 @@
         <div class="temoignage_photo_fond sliderMob__logo">
         </div>
         <div class="temoignage_contenu sliderMob__texte">
-                <img src="./images/icone_communication.svg" class="deskOnly temoignage_icone"/>
+                <span class="deskOnly barre_couleur" data-color="orange"></span>
+                <img src="./images/icone_communication.svg" class="deskOnly temoignage_icone" width="100%"/>
                 <h3 class="deskOnly">Communication</h3>
                 <p>« MMI&Après a été une excellente occasion de mettre un premier pas dans le monde professionnel ! En plus d’avoir trouvé mon stage, l’événement m’a permis de prendre davantage confiance en moi. »</p>
         </div>
