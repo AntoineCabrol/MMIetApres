41ff7651fb9876b78a1b212cfb39acc6954c20d7
Antoine Cabrol <contact@antoinecabrol.com>
2018-11-28 19:59:00 +0100

    correctifs app

--- edition2018/mentions.php
+++ edition2018/mentions.php
@@ -1,9 +1,17 @@
 <?php include "includes/_head.php" ?>
 
 
-<body class="page-index page-mentions">
+<body class="page-mentions wrap1">
 
-  <?php include "includes/_mentions.php" ?>
+  <div class="presentation">
+
+    <a href="index.php">Retour</a>
+
+    <h1>Mentions légales</h1>
+
+    <?php include "includes/_mentions.php" ?>
+
+  </div>
 
   <!-- JQUERY -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
