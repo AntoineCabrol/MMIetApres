3ab01cf1f9f433a3f220b843b6adc301dd60d518
BertrandPassieux <jean-francois.passieux@orange.fr>
2018-11-29 15:52:08 +0100

    [mention]

--- edition2018/includes/_mentions.php
+++ edition2018/includes/_mentions.php
@@ -1,12 +1,31 @@
 <article class="mentions">
 
-<p>
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  <h4>Emetteurs</h4>
+  <h4>Hébergement</h4>
+  <p>Hébergement réalisé par l'Université de Savoie.</p>
+  <h4>Conception du site et conception graphique.</h4>
+  <p>Agence Neon</p>
+  <h4>Législation</h4>
+  <p>Conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez d'un droit d'accès aux informations vous concernant.
 </p>
+  <a href="secretariat.src@univ-savoie.fr">secretariat.src@univ-savoie.fr</a>
+  <p>Département MMI, IUT de CHAMBERY, Université de Savoie</p>
+  <span></span>
+  <p>IUT de Chambéry</p>
+  <p>Campus Scientifique - Savoie Technolac</p>
+  <p>73376 Le Bourget du Lac Cedex</p>
+  <p>Les fichiers résultants ne sont jamais transmis à des tiers. En particulier, nous ne commercialisons aucun fichier d'adresses ou de courriels.
+</p>
+  <h4>Crédits photographiques</h4>
+  <span></span>
+  <p>Département MMI</p>
+  <h4>Crédits iconographiques</h4>
+  <span></span>
+  <p>Icones faites par Nikita Golubev de www.flaticon.com</p>
 
-<p>
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</p>
+
+
+
 
 
 </article>
