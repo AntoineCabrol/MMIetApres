8f45c56691a9b7acf7fd94f3a9256d51c461f735
BertrandPassieux <jean-francois.passieux@orange.fr>
2018-11-29 15:17:52 +0100

    [etudiants]photo+nom

--- edition2018/includes/_etudiants.php
+++ edition2018/includes/_etudiants.php
@@ -1,4 +1,4 @@
-<section class="etudiants">
+<section class="etudiants" id="etudiants">
 
   <h1 class="deskOnly h_etudiants">Témoignages</h1>
 
@@ -12,15 +12,15 @@
             <img src="./images/icone_audiovisuel.svg" class="deskOnly temoignage_icone" width="100%"/>
             <h3 class="deskOnly">Audiovisuel</h3>
             <p class="temoignage_texte">
-              « J’avais trouvé un stage dans une boite de production de pub à
-              Paris, mais j’ai quand même souhaité me mesurer au stage-dating que
-              proposait MMI&Après. J’ai fais la rencontre de Julien, directeur
-              général et réalisateur de l’Agence Future. Je me suis rendue compte
-              que nos attentes et
-              que les missions qu'il attendait me correspondaient. Quelques heures
+              « J’avais trouvé un stage dans une boite à
+              Paris, mais j’ai quand même souhaité me mesurer au stage-dating de MMI&Après.
+              J’ai rencontré Julien, directeur
+              général et réalisateur de l’Agence Future. Nos attentes et
+              les missions qu'il attendait me correspondaient. Quelques heures
               plus tard, Julien me recontactait pour me proposer de faire parti de
               l’aventure ! »
             </p>
+          <p class="temoignage_nom">Marie Raison</p>
         </div>
     </div>
     <div class="temoignage_block sliderMob__elem" data-S4="programmation">
@@ -30,7 +30,12 @@
                 <span class="deskOnly barre_couleur" data-color="vert"></span>
                 <img src="./images/icone_programmation.svg" class="deskOnly temoignage_icone" width="100%"/>
                 <h3 class="deskOnly">Programmation</h3>
-                <p>« J’avais déjà repéré l’entreprise NewQuest avant l’événement, mais c’est pendant MMI&Après que j’ai passé mon stage-dating. Un peu de pression et pas mal d’échanges avec les entreprises, qui m’ont permis de décrocher mon stage à Newquest, et ainsi obtenir une alternance ! »</p>
+                <p>« J’avais déjà repéré l’entreprise NewQuest avant l’événement,
+                  mais c’est pendant MMI&Après que j’ai passé mon stage-dating.
+                  Un peu de pression et pas mal d’échanges avec les entreprises,
+                  qui m’ont permis de décrocher mon stage à Newquest,
+                  et ainsi obtenir une alternance ! »</p>
+                  <p class="temoignage_nom">Clément Dumas</p>
         </div>
     </div>
     <div class="temoignage_block sliderMob__elem" data-S4="graphisme">
@@ -40,7 +45,13 @@
             <span class="deskOnly barre_couleur" data-color="magenta"></span>
             <img src="./images/icone_graphisme.svg" class="deskOnly temoignage_icone" width="100%"/>
             <h3 class="deskOnly">Graphisme</h3>
-            <p>« Je suis allé à MMI&Après afin de trouver un stage car il y avait des agences qui m'intéressaient. J'ai pu passer quatre entretiens qui m'ont permis de discuter avec des professionels. Deux professionnels étaient intéressés. J'ai pu visiter leur boite et repasser un entretien plus long. MMI&Après permet d'avoir des rendez-vous avec les agences sans avoir à passer des heures à les chercher. De plus cela permet d'avoir des contacts. »</p>
+            <p>« Je suis allé à MMI&Après afin de trouver un stage.
+              J'ai pu passer quatre entretiens qui m'ont permis de discuter avec des professionels.
+              Deux étaient intéressés.
+              J'ai pu visiter leur boite et repasser un entretien plus long.
+              MMI&Après permet d'avoir des rendez-vous avec les agences sans avoir à
+              passer des heures à les chercher. De plus cela permet d'avoir des contacts. »</p>
+              <p class="temoignage_nom">Anna Levy</p>
         </div>
     </div>
     <div class="temoignage_block sliderMob__elem" data-S4="communication">
@@ -50,17 +61,20 @@
                 <span class="deskOnly barre_couleur" data-color="orange"></span>
                 <img src="./images/icone_communication.svg" class="deskOnly temoignage_icone" width="100%"/>
                 <h3 class="deskOnly">Communication</h3>
-                <p>« MMI&Après a été une excellente occasion de mettre un premier pas dans le monde professionnel ! En plus d’avoir trouvé mon stage, l’événement m’a permis de prendre davantage confiance en moi. »</p>
+                <p>« MMI&Après a été une excellente occasion de mettre un premier pas dans
+                  le monde professionnel ! En plus d’avoir trouvé mon stage,
+                  l’événement m’a permis de prendre davantage confiance en moi. »</p>
+                  <p class="temoignage_nom">Elodie Spicher</p>
         </div>
     </div>
   </div>
 
-  <div class="deskOnly flex_button_tem">
+  <!-- <div class="deskOnly flex_button_tem">
     <a class="bouton inscription_etudiants">Étudiants</a>
     <a class="bouton inscription_professionels">Professionnels</a>
-  </div>
+  </div> -->
 
-  <div class="mobileOnly etPart etPart--Toi">
+  <div class="etPart etPart--Toi">
 
     <svg class="etPart__icone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 171.735 120.033"><defs><style>.a,.b{fill:#dfdfdf;}.b{font-size:56px;font-family:Roboto-Black, Roboto;font-weight:800;}</style></defs><g transform="translate(-58.265 -391)"><path class="a" d="M58.006,117.646V107.509c0-4.868,0-4.868,4.8-4.868H81.749c1.774,0,2.589.479,2.447,2.4a70.373,70.373,0,0,0,0,7.574c.091,1.722-.583,2.188-2.188,2.11-2.227-.117-4.466.116-6.68-.078s-2.706.88-2.68,2.732c0,2.382.078,4.764,0,7.133a14.4,14.4,0,0,1-.583,4.156,3.4,3.4,0,0,0,1.075,3.884c4.324,4.182,8.545,8.467,12.869,12.648,1.295,1.217,1.295,2.084,0,3.275q-4.648,4.492-9.14,9.14c-1.295,1.295-2.253,1.295-3.521,0-2.887-2.991-5.891-5.865-8.752-8.881-1.295-1.4-2.317-1.489-3.638,0-12.1,13.309-37,14.772-51.177,1.1A30.8,30.8,0,0,1,.006,128.12c-.4-10.642,3.78-19.316,11.263-26.579,1.295-1.2.842-1.89-.142-2.874A25.673,25.673,0,0,1,6.6,67.906,26.281,26.281,0,0,1,34.029,54.675,25.4,25.4,0,0,1,55,75.6c1.761,10.435-1.7,18.979-9.995,25.543-2.343,1.851-2.343,1.838-.285,3.884,3.573,3.664,7.121,7.354,10.694,11.03.608.621,1.295,1.191,1.89,1.787ZM34.961,141.8c5.774-.142,10.525-1.8,14.241-5.878,1.01-1.1,1.062-1.942-.078-2.978-1.644-1.489-3.2-3.094-4.79-4.648-5.541-5.36-11.069-10.733-16.662-16.028a2.589,2.589,0,0,0-2.2-.583c-5.619,2.887-9.179,11.432-7.457,17.633C20.034,136.69,26.663,141.493,34.961,141.8Zm-5.45-51.863a9.878,9.878,0,0,0,9.541-9.775,9.774,9.774,0,0,0-9.606-9.425,9.619,9.619,0,0,0,.065,19.225Z" transform="translate(58.285 351.612)"/><text class="b" transform="translate(111 444)"><tspan x="0" y="0">Toi ?</tspan></text></g></svg>
     <div class="etPart__texte">
